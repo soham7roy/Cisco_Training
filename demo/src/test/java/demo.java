a739daa2641dbf34ddabd27b5ba46754035bd915
soham.roy01 <soham.roy01@HYDPCM346075L.ad.infosys.com>
2020-10-27 18:46:01 +0530

    Maven task Project

---
+++ demo/src/test/java/demo.java
@@ -0,0 +1,26 @@
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+
+public class demo {
+	WebDriver driver;
+	String url="https://www.google.com/";
+  @Test
+  public void f() {
+	  driver.get(url);
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+	  System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\Google\\Chrome\\Application\\chromedriver.exe");
+			driver=new ChromeDriver();
+			
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+	  driver.quit();
+  }
+
+}
